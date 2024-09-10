ac2c5075a29af84d3ca9bfd8b865e4f8272a423e
bepyan <bepyan@naver.com>
2024-09-10 19:12:30 +0900

    style: cleanup

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,7 +1,6 @@
 import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
-import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const baseDir = pathToRoot(fileData.slug!)
